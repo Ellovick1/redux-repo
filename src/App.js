fa128da5c713c06be847529d4a92f097b6009641
Ellovick1 <ezekielelom1@gmail.com>
2023-12-11 23:41:44 +0100

    created redux repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Count from "./components/Count";
+import Post from "./components/Post";
+
+
+function App() {
+  return (
+    <div className="App">
+       <Count />
+       <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/reducers/countReducer.js
@@ -0,0 +1,46 @@
+import {inc,dec,reset} from '../actions/countAction'
+
+const initialState = { 
+    count : 30,
+    posts : [
+        {
+            id: 1,
+            title:'rice',
+            desc: 'rice with meat'
+        },
+        {
+            id: 2,
+            title:'beans',
+            desc: 'beans with dodo'
+        },
+        {
+            id: 3,
+            title:'pizza',
+            desc: 'pizza is cool'
+        },
+    ]
+}
+
+export const countReducer = (state = initialState,action) => { 
+     switch(action.type) { 
+        case inc : 
+            console.log('increment is reached');
+             return { 
+              count : state.count +1
+            }
+         break;
+        case dec : 
+        console.log('decrement is reached');
+            return { 
+               count : state.count -1
+              }
+         break;
+        case reset:
+             console.log('reset is reached');
+              return initialState
+         break;
+         default: 
+               return state
+
+     }
+}
